0d764c38d042d78e095f66a815479d59193f8c4e
Nikolay Marzakov <114495254+nvmarzakov@users.noreply.github.com>
2024-07-22 07:10:00 +0300

    created relationship one to many
    - one book have many reviews

--- Book-Review/database/migrations/2024_07_22_033738_create_reviews_table.php
+++ Book-Review/database/migrations/2024_07_22_033738_create_reviews_table.php
@@ -14,10 +14,15 @@ return new class extends Migration
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
 
+            $table->unsignedTinyInteger('book_id');
+
             $table->text('review');
             $table->unsignedTinyInteger('rating');
 
             $table->timestamps();
+
+            $table->foreign('book_id')->references('id')->on('books')
+                ->onDelete('cascade');
         });
     }
 
